4a343a8d1230fc0a0dfe47a3825cd94fe4a22813
Võ Đại Hiệp <vodaihiep209@gmail.com>
2024-02-19 16:22:26 +0700

    in năm âm lịch

--- pythonProjectTest/ViDu1.py
+++ pythonProjectTest/ViDu1.py
@@ -1,1 +1,12 @@
 #Nhập 1 năm dương lịch đổi ra năm âm lịch
+can = ['Canh', 'Tân', 'Nhâm', 'Quý', 'Giáp', 'Ất', 'Binh', 'Đinh', 'Mậu', 'Kỷ']
+chi = ['Thân', 'Dậu', 'Tuất', 'Hợi', 'Tý', 'Sửu', 'Dần', 'Mẹo', 'Thìn', 'Tỵ', 'Ngọ', 'Mùi']
+#Nhập năm dương lịch
+namDL = int(input("Nhập năm dương lịch: "))
+#Tính can
+can1 = namDL % 10
+#Tính chi
+chi1 = namDL % 12
+#Đổi ra năm âm lịch
+namAL = can[can1] + " " + chi[chi1]
+print(namAL)
