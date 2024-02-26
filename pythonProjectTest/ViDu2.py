99798c21595148199b54fb6aaf2cf8f78c28dd2e
Võ Đại Hiệp <vodaihiep209@gmail.com>
2024-02-26 14:57:43 +0700

    ví dụ về kiểu chuỗi

---
+++ pythonProjectTest/ViDu2.py
@@ -0,0 +1,8 @@
+st = "Nha Trang Khánh Hòa"
+print(len(st))
+
+words = st.split(" ")
+print(words)
+
+myList = [1984, "Nha Trang", 3.14] #có thể thay đổi
+myTuple = ('Hello', 3.14, 8) #được sắp xếp theo thứ tự ko thể thay đổi
