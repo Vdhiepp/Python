be1822d37fc275b7d8542a223fedd8d9babf1ae0
Võ Đại Hiệp <vodaihiep209@gmail.com>
2024-03-04 15:29:21 +0700

    Create ViDu6.py

---
+++ pythonProjectTest/ViDu6.py
@@ -0,0 +1,4 @@
+#Một máy ATM có các loại tiền: 100, 20 và 5 USD. Một người cần rút số tiền m.
+##a, Tìm phương án rút tốt nhất (có ít tờ tiền nhất)
+##b, Liệt kê tất cả các cách rút được số tiền m. Có tất cả bao nhiêu cách?
+
