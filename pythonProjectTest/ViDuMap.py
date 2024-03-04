e18369dcbaea12a7e6c631aba09f475ee2dfa35a
Võ Đại Hiệp <vodaihiep209@gmail.com>
2024-03-04 16:58:54 +0700

    ví dụ về hàm map()

---
+++ pythonProjectTest/ViDuMap.py
@@ -0,0 +1,16 @@
+#Cho dãy a[1,2,3,4] tạo dãy b chứa các phần tử là bình phương của phần tử tương ứng của dãy a, b=[1,4,9,16]
+
+#Cách 1:
+a = [1,2,3,4]
+b = []
+for i in range(len(a)):
+    b.append(a[i]**2)
+print(b)
+
+#Cách 2: Dùng hàm map
+#Định nghĩa hàm bình phương
+def Square(x):
+    return x**2
+b = map(Square, a)
+b = list(b)
+print(b)
